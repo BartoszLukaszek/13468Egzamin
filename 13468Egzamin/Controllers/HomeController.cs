f2c80f3cffc3ea9ccef8092495130000f9c2f208
BartoszLukaszek <barti120@onet.pl>
2022-02-02 00:41:07 +0100

    test

--- 13468Egzamin/Controllers/HomeController.cs
+++ 13468Egzamin/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace _13468Egzamin.Controllers
         {
             return View();
         }
+        public IActionResult Game()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
